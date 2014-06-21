25baab7ee43be2affc6b9fb8408e09f9204e1b71
nikakhov <sharni@vt.edu>
2014-06-20 21:50:16 -0400

    More gravatar features.  Forward to create account

--- vendor/js/angularjs-gravatardirective.js
+++ vendor/js/angularjs-gravatardirective.js
@@ -33,48 +33,59 @@ angular.module('angularjs-gravatardirective.directives')
         return {
             restrict:"EAC",
             link:function (scope, elm, attrs) {
+                getGravatarImage(attrs.gravatarEmail);
+                //scope.$apply();
                 // by default the values will come in as undefined so we need to setup a
                 // watch to notify us when the value changes
                 scope.$watch(attrs.gravatarEmail, function (value) {
                     // let's do nothing if the value comes in empty, null or undefined
                     if ((value !== null) && (value !== undefined) && (value !== '') && (null !== value.match(/.*@.*\..{2}/))) {
-                        // parse the size attribute
-                        var size = attrs.gravatarSize || 40;
-                        // parse the ratings attribute
-                        var rating = attrs.gravatarRating || 'pg';
-                        // parse the default image url
-                        var defaultUrl = attrs.gravatarDefault || 'identicon';
-                        // parse css class
-                        var cssClass = attrs.gravatarCssClass || 'gravatar-icon';
-                        // get image src from service
-                        var grv = gravatarImageService.getImageSrc(value, size, rating, defaultUrl, attrs.gravatarSecure);
-                        var src = grv.src;
-                        var hash = grv.hash;
-                        // construct the tag to insert into the element
-                        var tag = '<img class="' + cssClass + '" src="' + src + '" >';
-                        //remove any existing imgs 
-                        elm.find('img').remove();
-
-
                         
-
+                        getGravatarImage(value);
                         // Get Gravatar Displayname
                         //  remove the previous name
-                        scope.gravatarDisplayName="";
-                        var url =  'http://www.gravatar.com/' + hash + '.json';
-                        $.getJSON(url + "?callback=?", null, function(data) {
-                            scope.gravatarDisplayName=data.entry[0].displayName;
-                            scope.$apply();
-                        });
-
-                        // insert the tag into the element
-                        elm.append(tag);
-                        // add handler to remove if image fails to load
-                        elm.find('img').bind('error', function() {
-                            elm.find('img').remove();
-                        });
+                        
                     }
                 });
+
+                function getGravatarImage (value) {
+                    // parse the size attribute
+                    var size = attrs.gravatarSize || 40;
+                    // parse the ratings attribute
+                    var rating = attrs.gravatarRating || 'pg';
+                    // parse the default image url
+                    var defaultUrl = attrs.gravatarDefault || 'identicon';
+                    // parse css class
+                    var cssClass = attrs.gravatarCssClass || 'gravatar-icon';
+                    // get image src from service
+                    var grv = gravatarImageService.getImageSrc(value, size, rating, defaultUrl, attrs.gravatarSecure);
+                    var src = grv.src;
+                    var hash = grv.hash;
+                    // construct the tag to insert into the element
+                    var tag = '<img class="' + cssClass + '" src="' + src + '" >';
+                    //remove any existing imgs 
+                    elm.find('img').remove();
+
+
+                    scope.gravatarDisplayName="Gravatar unknown";
+                    var url =  'http://www.gravatar.com/' + hash + '.json';
+                    $.getJSON(url + "?callback=?", null, function(data) {
+                        scope.gravatarDisplayName=data.entry[0].displayName;
+                        scope.$apply();
+                    });
+
+
+
+                    // insert the tag into the element
+                    elm.append(tag);
+                    // add handler to remove if image fails to load
+                    elm.find('img').bind('error', function() {
+                        elm.find('img').remove();
+                    });
+
+
+
+                }
             }};
     }]);
 angular.module('angularjs-gravatardirective.services')
